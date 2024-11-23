fe6672c474695f78163d1e8d65b0e53f95ffb9ab
nacattle <nacattle@uno.edu>
2024-11-22 18:09:19 -0600

    added navigators to scrren for app.js

--- App.js
+++ App.js
@@ -1,17 +1,26 @@
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import { createStackNavigator} from "react-navigation-stack";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import HomeScreen from "./src/screens/HomeScreen";
 
-const navigator = createStackNavigator(
-  {
-    Home: HomeScreen,
-  },
-  {
-    initialRouteName: "Home",
-    defaultNavigationOptions: {
-      title: "App",
+const switchNavigator = createSwitchNavigator({
+  loginFlow: 
+    { Signup: SignupScreen,
+      Signin: SigninScreen,
     },
-  }
-);
+  mainFlow: createMaterialBottomTabNavigator({ 
+    restaurantSearchFlow: createStackNavigator({
+      Home: HomeScreen,
+      Search: SearchScreen,
+      Restaurant: RestaurantHomeScreen,
+      RestaurantMenu: RestaurantMenuScreen,
+      Item: ItemReviewScreen,
+      UserItemReview: UserItemReviewScreen,
+      UserProfile: UserProfileScreen,
+    }),
+    Search: SearchScreen,
+    Account: AccountScreen
+  }),
+});
 
-export default createAppContainer(navigator);
+export default createAppContainer(switchNavigator);
